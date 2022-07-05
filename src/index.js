b66ea23c962c161032888ae03a44546f929fd939
js3g20 <js3g20@soton.ac.uk>
2022-07-05 22:25:46 +0100

    Add Routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="home" element={<Home />} />
+    <Route path="*" element={<Navigate to="home" />} />
+  </Routes>
+</BrowserRouter>
 );
