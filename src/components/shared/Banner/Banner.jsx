171255670a178af3786353d270611544c587e36f
js3g20 <js3g20@soton.ac.uk>
2022-07-06 22:55:17 +0100

    Update Banner

--- src/components/shared/Banner/Banner.jsx
+++ src/components/shared/Banner/Banner.jsx
@@ -1,13 +1,37 @@
+import { Button, Container, Stack } from '@chakra-ui/react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Banner = () => {
 
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Hello World!
-        </div>
+        <Container>
+            <Stack direction='row' spacing={4} align='center'>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/home')}}>
+                    Home
+                </Button>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/education')}}>
+                    Education
+                </Button>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/work')}}>
+                    Experience
+                </Button>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/project')}}>
+                    Projects
+                </Button>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/music')}}>
+                    Music
+                </Button>
+                <Button variant='ghost' size='sm' onClick={() => {navigate('/contact')}}>
+                    Contact
+                </Button>
+            </Stack>
+            
+        </Container>
     );
 }
 
--- src/components/shared/Banner/LargeBanner.jsx
+++ src/components/shared/Banner/LargeBanner.jsx
@@ -1,13 +1,22 @@
+import { Container, Divider, Text } from '@chakra-ui/react';
 import React from 'react';
+import Banner from './Banner';
 
 
 
 const LargeBanner = () => {
 
     return (
-        <div>
-            Hello World!
-        </div>
+        <Container centerContent>
+            <Text fontSize='3xl'>
+                James Stockton
+            </Text>
+            <Text>
+                Electrical and Electronic Engineering Student
+            </Text>
+            <Divider />
+            <Banner />
+        </Container>
     );
 }
 
