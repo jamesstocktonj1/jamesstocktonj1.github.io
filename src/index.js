380fe6e7d3bfc7890bf22b8d7500793091fe9fc4
js3g20 <js3g20@soton.ac.uk>
2022-07-19 20:15:27 +0100

    Update Index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Project from './components/Project/Project';
 import ProjectHome from './components/Project/ProjectHome';
@@ -21,7 +21,6 @@ root.render(
       <Route path="project" element={<ProjectHome />} />
       <Route path="project/:article" element={<Project />} />
       <Route path="contact" element={<Home />} />
-
       <Route path="*" element={<Navigate to="home" />} />
     </Routes>
   </HashRouter>
